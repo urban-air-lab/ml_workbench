013bc4f552caa9e43d836becec139a3737294932
garc <marc@golder.de>
2024-12-27 14:27:47 +0100

    remove dummy values

--- utils.py
+++ utils.py
@@ -13,10 +13,11 @@ class LUBWData:
         self.datasonta.set_index("_time", inplace=True)
         self.datasonta.index = pd.to_datetime(
             self.datasonta.index, format = "%Y-%m-%d %H:%M:%S+00:00")
-        self.datasonta = self.datasonta[["data_RAW_ADC_NO2_A","data_RAW_ADC_NO2_W"]]
+        self.datasonta["current_diff"] = self.datasonta["data_RAW_ADC_NO2_W"] - self.datasonta["data_RAW_ADC_NO2_A"]
+        self.datasonta = self.datasonta[["current_diff"]]
 
         self.all_data = self.dataLUBW.join(self.datasonta, how="inner", lsuffix="_LUBW", rsuffix="sonta")
-
+        self.all_data = self.all_data[self.all_data["NO2"] != -999]
 
 data = LUBWData()
 print(data.all_data)
