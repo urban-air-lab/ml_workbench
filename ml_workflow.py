ee56f6adfa2fd78ebe60bb9bb88e88637f8c4312
garc <marc@golder.de>
2024-12-27 19:54:40 +0100

    added deviation

--- ml_workflow.py
+++ ml_workflow.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import legend
 from sklearn.model_selection import train_test_split
 import keras
 import matplotlib.pyplot as plt
@@ -14,19 +15,18 @@ model.add(keras.layers.Dense(units=16, activation="relu"))
 model.add(keras.layers.Dense(units=8, activation="relu"))
 model.add(keras.layers.Dense(units=1))
 
-model.compile(optimizer=keras.optimizers.Adam(0.001) ,loss='mean_squared_error')
+model.compile(optimizer=keras.optimizers.Adam(0.001) ,loss="mean_squared_error")
 model.fit(x_train, y_train, epochs=10, batch_size=3)
 
 predictions = model.predict(x_test)
+deviation = y_test - predictions.flatten()
 
-fig, axes = plt.subplots(2, 1)
-axes[0].plot(y_test)
-axes[1].plot(predictions)
-plt.savefig('./results_sontc.png')
+fig, axes = plt.subplots(3, 1)
+axes[0].plot(y_test, label="y_test")
+axes[1].plot(predictions, label="predictions")
+axes[2].plot(deviation, label="y_test - predicition")
+for ax in axes:
+    ax.grid(True)
+    ax.legend()
+plt.savefig('./results/results_sontc.png')
 plt.show()
-
-
-
-
-
-
