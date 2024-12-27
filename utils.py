b94cd1b0d107efdfaa109121a9d789841a89c3c6
garc <marc@golder.de>
2024-12-27 14:55:41 +0100

    refactoring

--- utils.py
+++ utils.py
@@ -1,32 +1,37 @@
 import pandas as pd
 
-class LUBWData:
-
+class SensorData:
     def __init__(self):
-        self.dataLUBW = pd.read_csv("./data/minute_data_lubw.csv")
-        self.dataLUBW.set_index("Zeitstempel", inplace=True)
-        self.dataLUBW.index = pd.to_datetime(
-            self.dataLUBW.index, format = "%Y-%m-%d %H:%M:%S")
-        self.dataLUBW = self.dataLUBW[["NO2"]]
+        self.dataLUBW = self.__read_lubw()
+        self.datasonta = self.__read_sonta()
 
-        self.datasonta = pd.read_csv("./data/sont_a_data.csv")
-        self.datasonta.set_index("_time", inplace=True)
-        self.datasonta.index = pd.to_datetime(
-            self.datasonta.index, format = "%Y-%m-%d %H:%M:%S+00:00")
         self.datasonta["current_diff"] = self.datasonta["data_RAW_ADC_NO2_W"] - self.datasonta["data_RAW_ADC_NO2_A"]
         self.datasonta = self.datasonta[["current_diff"]]
 
         self.all_data = self.dataLUBW.join(self.datasonta, how="inner", lsuffix="_LUBW", rsuffix="sonta")
         self.all_data = self.all_data[self.all_data["NO2"] != -999]
 
-    def get_data(self):
-        return self.all_data
+    def __read_lubw(self):
+        dataLUBW = pd.read_csv("./data/minute_data_lubw.csv")
+        dataLUBW.set_index("Zeitstempel", inplace=True)
+        dataLUBW.index = pd.to_datetime(
+            dataLUBW.index, format="%Y-%m-%d %H:%M:%S")
+        return dataLUBW[["NO2"]]
 
+    def __read_sonta(self):
+        datasonta = pd.read_csv("./data/sont_a_data.csv")
+        datasonta.set_index("_time", inplace=True)
+        datasonta.index = pd.to_datetime(
+        datasonta.index, format="%Y-%m-%d %H:%M:%S+00:00")
+        return datasonta
+
+    @property
     def get_NO2(self):
         return self.all_data["NO2"]
 
-    def get_current_diff(self):
+    @property
+    def get_difference_electrodes_no2(self):
         return self.all_data["current_diff"]
 
-data = LUBWData()
-print()
+data = SensorData()
+print(data.get_NO2)
