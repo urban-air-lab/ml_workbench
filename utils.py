aaa667494a0e21e93e182805a0c71014dc9a0240
garc <marc@golder.de>
2024-12-28 09:08:53 +0100

    added loading of futuredata for further testing

--- utils.py
+++ utils.py
@@ -4,9 +4,13 @@ class SensorData:
     def __init__(self):
         self.dataLUBW = self.__read_lubw()
         self.datasonta = self.__read_sonta()
+        self.futuredata = self.__read_futuredata()
 
         self.datasonta["current_diff"] = self.datasonta["data_RAW_ADC_NO2_W"] - self.datasonta["data_RAW_ADC_NO2_A"]
         self.datasonta = self.datasonta[["current_diff"]]
+
+        self.futuredata["current_diff"] = self.futuredata["data_RAW_ADC_NO2_W"] - self.futuredata["data_RAW_ADC_NO2_A"]
+        self.futuredata = self.futuredata[["current_diff"]]
 
         self.all_data = self.dataLUBW.join(self.datasonta, how="inner", lsuffix="_LUBW", rsuffix="sonta")
         self.all_data = self.all_data[self.all_data["NO2"] != -999]
@@ -22,8 +26,15 @@ class SensorData:
         datasonta = pd.read_csv("./data/sont_c_data.csv")
         datasonta.set_index("_time", inplace=True)
         datasonta.index = pd.to_datetime(
-        datasonta.index, format="%Y-%m-%d %H:%M:%S+00:00")
+           datasonta.index, format="%Y-%m-%d %H:%M:%S+00:00")
         return datasonta
+
+    def __read_futuredata(self):
+        futuredata = pd.read_csv("./data/sont_c_20241211-202241228.csv")
+        futuredata.set_index("_time", inplace=True)
+        futuredata.index = pd.to_datetime(
+           futuredata.index, format="%Y-%m-%d %H:%M:%S+00:00")
+        return futuredata
 
     @property
     def get_NO2(self):
