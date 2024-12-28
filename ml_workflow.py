f0fe00ce1360d0f27d821ab8641694cc061e17ef
garc <marc@golder.de>
2024-12-28 09:46:24 +0100

    using the trained network on a future dataset and comparing with reference station

--- ml_workflow.py
+++ ml_workflow.py
@@ -1,10 +1,12 @@
 from sklearn.model_selection import train_test_split
 import keras
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from utils import SensorData
 
 data = SensorData()
+futuredata_lubw = pd.read_csv("./data/DEBW015_20241211-202241228.csv")
 
 x_train, x_test, y_train, y_test = train_test_split(data.get_difference_electrodes_no2, data.get_NO2, test_size=0.2, shuffle=False)
 
@@ -20,12 +22,19 @@ model.fit(x_train, y_train, epochs=10, batch_size=3)
 predictions = model.predict(x_test)
 deviation = y_test - predictions.flatten()
 
-fig, axes = plt.subplots(3, 1)
+futuredata_hourly = data.futuredata.resample("h").mean()
+further_prediction = model.predict(futuredata_hourly)
+
+fig, axes = plt.subplots(2, 2)
+axes = axes.flatten()
 axes[0].plot(y_test, label="y_test")
 axes[1].plot(predictions, label="predictions")
 axes[2].plot(deviation, label="y_test - predicition")
+axes[3].plot(further_prediction, label="11. dezember ff sont_c")
+axes[3].plot(futuredata_lubw.data_NO2, label="11. dezember ff LUBW")
 for ax in axes:
     ax.grid(True)
     ax.legend()
-plt.savefig('./results/results_sontc.png')
+# plt.savefig('./results/results_sontc.png')
 plt.show()
+pass
