933444d789bc62f465544ec67773d1e3953c29be
tobias.weiland <tobias.weiland@7p-group.com>
2024-12-28 21:12:43 +0100

    add typ hints

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class SensorData:
     def __init__(self):
         self.dataLUBW = self.__read_lubw()
@@ -15,21 +16,21 @@ class SensorData:
         self.all_data = self.dataLUBW.join(self.datasonta, how="inner", lsuffix="_LUBW", rsuffix="sonta")
         self.all_data = self.all_data[self.all_data["NO2"] != -999]
 
-    def __read_lubw(self):
+    def __read_lubw(self) -> pd.DataFrame:
         dataLUBW = pd.read_csv("./data/minute_data_lubw.csv")
         dataLUBW.set_index("Zeitstempel", inplace=True)
         dataLUBW.index = pd.to_datetime(
             dataLUBW.index, format="%Y-%m-%d %H:%M:%S")
         return dataLUBW[["NO2"]]
 
-    def __read_sonta(self):
+    def __read_sonta(self) -> pd.DataFrame:
         datasonta = pd.read_csv("./data/sont_c_data.csv")
         datasonta.set_index("_time", inplace=True)
         datasonta.index = pd.to_datetime(
            datasonta.index, format="%Y-%m-%d %H:%M:%S+00:00")
         return datasonta
 
-    def __read_futuredata(self):
+    def __read_futuredata(self) -> pd.DataFrame:
         futuredata = pd.read_csv("./data/sont_c_20241211-202241228.csv")
         futuredata.set_index("_time", inplace=True)
         futuredata.index = pd.to_datetime(
@@ -37,10 +38,10 @@ class SensorData:
         return futuredata
 
     @property
-    def get_NO2(self):
+    def get_NO2(self) -> pd.DataFrame:
         return self.all_data["NO2"]
 
     @property
-    def get_difference_electrodes_no2(self):
+    def get_difference_electrodes_no2(self) -> pd.DataFrame:
         return self.all_data["current_diff"]
 
