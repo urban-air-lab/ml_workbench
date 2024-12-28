5f3d2eaf5079f16864b9c3047cfafef820cc2528
tobias.weiland <tobias.weiland@7p-group.com>
2024-12-28 21:33:48 +0100

    simplfy SensorData

--- utils.py
+++ utils.py
@@ -4,8 +4,8 @@ import pandas as pd
 class SensorData:
     def __init__(self):
         self.dataLUBW = self.__read_lubw()
-        self.datasonta = self.__read_sonta()
-        self.futuredata = self.__read_futuredata()
+        self.datasonta = self.__read_custom_data("./data/sont_c_data.csv")
+        self.futuredata = self.__read_custom_data("./data/sont_c_20241211-202241228.csv")
 
         self.datasonta["current_diff"] = self.datasonta["data_RAW_ADC_NO2_W"] - self.datasonta["data_RAW_ADC_NO2_A"]
         self.datasonta = self.datasonta[["current_diff"]]
@@ -23,25 +23,18 @@ class SensorData:
             dataLUBW.index, format="%Y-%m-%d %H:%M:%S")
         return dataLUBW[["NO2"]]
 
-    def __read_sonta(self) -> pd.DataFrame:
-        datasonta = pd.read_csv("./data/sont_c_data.csv")
-        datasonta.set_index("_time", inplace=True)
-        datasonta.index = pd.to_datetime(
-           datasonta.index, format="%Y-%m-%d %H:%M:%S+00:00")
-        return datasonta
-
-    def __read_futuredata(self) -> pd.DataFrame:
-        futuredata = pd.read_csv("./data/sont_c_20241211-202241228.csv")
-        futuredata.set_index("_time", inplace=True)
-        futuredata.index = pd.to_datetime(
-           futuredata.index, format="%Y-%m-%d %H:%M:%S+00:00")
-        return futuredata
+    def __read_custom_data(self, file_path: str) -> pd.DataFrame:
+        data = pd.read_csv(file_path)
+        data.set_index("_time", inplace=True)
+        data.index = pd.to_datetime(
+           data.index, format="%Y-%m-%d %H:%M:%S+00:00")
+        return data
 
     @property
-    def get_NO2(self) -> pd.DataFrame:
+    def get_NO2(self) -> pd.Series:
         return self.all_data["NO2"]
 
     @property
-    def get_difference_electrodes_no2(self) -> pd.DataFrame:
+    def get_difference_electrodes_no2(self) -> pd.Series:
         return self.all_data["current_diff"]
 
