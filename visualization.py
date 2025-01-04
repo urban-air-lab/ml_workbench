b99fd64b1d19aff3d9dd8b9d180ff67db5259381
tobias.weiland <tobias.weiland@7p-group.com>
2025-01-04 20:00:48 +0100

    delete visul file

--- visualization.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from utils import SensorData
-
-data = SensorData()
-
-fig, axes = plt.subplots(2, 1)
-axes[0].plot(data.get_NO2.index, data.get_NO2)
-axes[1].plot(data.get_difference_electrodes_no2.index, data.get_difference_electrodes_no2)
-plt.show()
