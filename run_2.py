f0e1faf92741abde3d1515f3111574c82feebe96
tobias.weiland <tobias.weiland@7p-group.com>
2025-01-04 19:56:26 +0100

    move december data to seperate workflow, delete old one

---
+++ run_2.py
@@ -0,0 +1,19 @@
+from utils import SensorData
+import keras
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    sontc_data_december = SensorData(file_path_lubw="./data/DEBW015_20241211-202241228.csv",
+                                   file_path_aqsn="./data/sont_c_20241211-202241228.csv",
+                                   in_hour=True)
+    feedforward_model = keras.saving.load_model("./model.keras")
+
+    december_prediction = feedforward_model.predict(sontc_data_december.get_difference_electrodes_no2)
+
+    fig, axes = plt.subplots(1)
+    axes.plot(sontc_data_december.get_NO2, label="11. dezember ff LUBW")
+    axes.plot(december_prediction, label="11. dezember ff sont_c")
+    axes.grid(True)
+    axes.legend()
+    plt.savefig('./results/results_sontc_december.png')
+    plt.show()
