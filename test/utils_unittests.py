d998ca830521fd0d0ae89360f767367197dcba75
tobias.weiland <tobias.weiland@7p-group.com>
2025-02-16 18:21:23 +0100

    fix bugs in config functions, add unittests

---
+++ test/utils_unittests.py
@@ -0,0 +1,33 @@
+import unittest
+from utils import _get_caller_directory, _get_config, get_database_config, get_workflow_config, get_lubw_config
+from pathlib import Path
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_get_caller_directory_test_is_in_path(self):
+        directory = _get_caller_directory(1)
+        self.assertIn("test", str(directory))
+
+    def test_get_config(self):
+        path = Path(_get_caller_directory(1) / "test.yaml")
+        config = _get_config(path)
+        self.assertEqual(config["name"], "test")
+
+    def test_get_database_config(self):
+        database_config = get_database_config()
+        self.assertEqual(database_config["url"], "test.com")
+
+    def test_get_workflow_config(self):
+        database_config = get_workflow_config()
+        self.assertEqual(database_config["name"], "test")
+
+    def test_get_lubw_config(self):
+        database_config = get_lubw_config()
+        self.assertEqual(database_config["name"], "test")
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import logging
 import yaml
 import matplotlib.pyplot as plt
 import seaborn as sns
+import inspect
+from pathlib import Path
 
 
 class SensorData:
@@ -71,24 +73,32 @@ def load_model(model_path: str) -> Model:
 
 
 def get_database_config():
-    __get_config("./database_config.yaml")
+    return _get_config(_get_caller_directory(2) / "database_config.yaml")
+
 
 def get_lubw_config():
-    __get_config("./lubw_config.yaml")
+    return _get_config(_get_caller_directory(2) / "lubw_config.yaml")
+
 
 def get_workflow_config():
-    __get_config("./workflow_config.yaml")
+    return _get_config(_get_caller_directory(2) / "workflow_config.yaml")
 
-def __get_config(path:str) -> dict:
+
+def _get_config(path: Path) -> dict:
     try:
-        with open(path, 'r') as file:
+        with open(str(path), 'r') as file:
             return yaml.safe_load(file)
     except FileNotFoundError:
-        logging.error(f"No workflow config found in directory")
+        logging.error(f"No config found in directory")
     except IOError:
         logging.error(f"IOError: An I/O error occurred")
     except Exception as e:
         logging.error(f"An unexpected error occurred: {e}")
+
+
+def _get_caller_directory(stack_position):
+    caller_file = inspect.stack()[stack_position].filename
+    return Path(caller_file).parent
 
 
 def plot_results(plot_path: str, *args) -> None:
