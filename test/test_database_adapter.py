c44417bdea31cbf16563cb004ede7023f810d5b9
tobias.weiland <tobias.weiland@7p-group.com>
2025-02-25 22:11:16 +0100

    add test for complete query flow to influx db

--- test/test_database_adapter.py
+++ test/test_database_adapter.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import pytest
-from database.database_adapter import InfluxDBAdapter
+from database.database_adapter import InfluxDBConnector
 
 
 @pytest.fixture
 def database_connection():
-    connection = InfluxDBAdapter("TestBucket")
+    connection = InfluxDBConnector("TestBucket")
     yield connection
 
 
@@ -16,15 +16,27 @@ def test_database_connection(database_connection):
     assert isinstance(query_result, list)
 
 
-def test_get_measurement(database_connection):
+def test_get_complete_query_as_dataframe(database_connection):
     bucket = "TestBucket"
     start_time = "2024-10-22T00:00:00Z"
     stop_time = "2024-10-22T23:00:00Z"
     measurement = "sont_c"
-    attribute = "CO"
+    attribute = ["CO", "NO"]
+
     query_result = database_connection.query_dataframe(f'''from(bucket: "{bucket}")
     |> range(start: {start_time}, stop: {stop_time})
     |> filter(fn: (r) => r._measurement == "{measurement}")
-    |> filter(fn: (r) => r._field == "{attribute}")''')
-    query_result.drop(["result", "table", "_field", "_measurement"], axis=1, inplace=True)
+    |> filter(fn: (r) => r._field == "{attribute[0]}" or r._field == "{attribute[1]}")''')
+
+    query_result.drop(["result", "table", "_measurement"], axis=1, inplace=True)
+    df0 = query_result[query_result["_field"] == attribute[0]]
+    df1 = query_result[query_result["_field"] == attribute[1]]
+
+    df0.reset_index(inplace=True)
+    df1.reset_index(inplace=True)
+
+    df_result = pd.DataFrame()
+    df_result[attribute[0]] = df0.loc[:, "_value"]
+    df_result[attribute[1]] = df1.loc[:, "_value"]
+
     print(query_result)
