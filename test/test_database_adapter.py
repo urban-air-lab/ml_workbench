d31e1b5994211d735b2a30ac22e70099437b34c3
tobias.weiland <tobias.weiland@7p-group.com>
2025-02-26 20:16:57 +0100

    extent database tests

--- test/test_database_adapter.py
+++ test/test_database_adapter.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pytest
-from database.database_adapter import InfluxDBConnector
+from database.database_connector import InfluxDBConnector
 
 
 @pytest.fixture
@@ -21,22 +21,19 @@ def test_get_complete_query_as_dataframe(database_connection):
     start_time = "2024-10-22T00:00:00Z"
     stop_time = "2024-10-22T23:00:00Z"
     measurement = "sont_c"
-    attribute = ["CO", "NO"]
+    attributes = ["CO", "NO"]
 
     query_result = database_connection.query_dataframe(f'''from(bucket: "{bucket}")
     |> range(start: {start_time}, stop: {stop_time})
     |> filter(fn: (r) => r._measurement == "{measurement}")
-    |> filter(fn: (r) => r._field == "{attribute[0]}" or r._field == "{attribute[1]}")''')
-
+    |> filter(fn: (r) => r._field == "{attributes[0]}" or r._field == "{attributes[1]}")''')
     query_result.drop(["result", "table", "_measurement"], axis=1, inplace=True)
-    df0 = query_result[query_result["_field"] == attribute[0]]
-    df1 = query_result[query_result["_field"] == attribute[1]]
-
-    df0.reset_index(inplace=True)
-    df1.reset_index(inplace=True)
 
     df_result = pd.DataFrame()
-    df_result[attribute[0]] = df0.loc[:, "_value"]
-    df_result[attribute[1]] = df1.loc[:, "_value"]
+
+    for attribute in attributes:
+        df = query_result[query_result["_field"] == attribute]
+        df.reset_index(inplace=True)
+        df_result[attribute] = df.loc[:, "_value"]
 
     print(query_result)
