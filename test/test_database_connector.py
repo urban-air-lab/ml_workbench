7d31579d6249a684aea4dd11750b23c3b67f2c09
tobias.weiland <tobias.weiland@7p-group.com>
2025-02-26 20:29:32 +0100

    switch to flux query build in test

--- test/test_database_connector.py
+++ test/test_database_connector.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import pytest
 from database.database_connector import InfluxDBConnector
+from database.influx_buckets import InfluxBuckets
+from database.influx_query_builder import InfluxQueryBuilder
 
 
 @pytest.fixture
@@ -17,16 +19,16 @@ def test_database_connection(database_connection):
 
 
 def test_get_complete_query_as_dataframe(database_connection):
-    bucket = "TestBucket"
-    start_time = "2024-10-22T00:00:00Z"
-    stop_time = "2024-10-22T23:00:00Z"
-    measurement = "sont_c"
     attributes = ["CO", "NO"]
 
-    query_result = database_connection.query_dataframe(f'''from(bucket: "{bucket}")
-    |> range(start: {start_time}, stop: {stop_time})
-    |> filter(fn: (r) => r._measurement == "{measurement}")
-    |> filter(fn: (r) => r._field == "{attributes[0]}" or r._field == "{attributes[1]}")''')
+    query = InfluxQueryBuilder()\
+        .set_bucket(InfluxBuckets.test_bucket.value) \
+        .set_range("2024-10-22T00:00:00Z", "2024-10-22T23:00:00Z") \
+        .set_measurement("sont_c") \
+        .set_fields(["CO", "NO"]) \
+        .build()
+
+    query_result = database_connection.query_dataframe(query)
     query_result.drop(["result", "table", "_measurement"], axis=1, inplace=True)
 
     df_result = pd.DataFrame()
