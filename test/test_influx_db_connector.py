62f30f0bab571634e9c0e68ee12a8072a6b51c89
tobias.weiland <tobias.weiland@7p-group.com>
2025-03-02 21:23:22 +0100

    remove database integrationtests from github actions

--- test/test_influx_db_connector.py
+++ test/test_influx_db_connector.py
@@ -1,7 +1,11 @@
+import os
+
 import pytest
 from database.Influx_db_connector import InfluxDBConnector
 from database.influx_buckets import InfluxBuckets
 from database.influx_query_builder import InfluxQueryBuilder
+
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
 
 @pytest.fixture
@@ -10,6 +14,7 @@ def database_connection():
     yield connection
 
 
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_database_connection(database_connection):
     # TestBucket data range is october 2024
     query_result = database_connection.query('''from(bucket: "TestBucket")
@@ -17,8 +22,9 @@ def test_database_connection(database_connection):
     assert isinstance(query_result, list)
 
 
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_get_complete_query_as_dataframe(database_connection):
-    query = InfluxQueryBuilder()\
+    query = InfluxQueryBuilder() \
         .set_bucket(InfluxBuckets.TEST_BUCKET.value) \
         .set_range("2024-10-22T00:00:00Z", "2024-10-22T23:00:00Z") \
         .set_measurement("sont_c") \
@@ -26,5 +32,4 @@ def test_get_complete_query_as_dataframe(database_connection):
         .build()
 
     query_result = database_connection.query_dataframe(query)
-    print(query_result) #TODO: extend testing
-
+    print(query_result)  # TODO: extend testing
