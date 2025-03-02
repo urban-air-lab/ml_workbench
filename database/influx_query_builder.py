7406aea6a0f9f5e2403b814309ffbbb1958b1f9e
tobias.weiland <tobias.weiland@7p-group.com>
2025-03-02 20:39:01 +0100

    renaming stuff

--- database/influx_query_builder.py
+++ database/influx_query_builder.py
@@ -12,51 +12,51 @@ class InfluxQueryBuilder:
     """
 
     def __init__(self):
-        self.bucket = None
-        self.range = None
-        self.measurement = None
-        self.fields = None
-        self.pivot = '''|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")'''
+        self._bucket = None
+        self._range = None
+        self._measurement = None
+        self._fields = None
+        self._pivot = '''|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")'''
         self.query = None
 
     def set_bucket(self, bucket: str):
-        self.bucket = f'''from(bucket: "{bucket}")'''
+        self._bucket = f'''from(bucket: "{bucket}")'''
         return self
 
-    def set_range(self, start_time: str, stop_time: str):
-        valid_start = self._is_valid_iso8601_utc(start_time)
-        valid_end = self._is_valid_iso8601_utc(stop_time)
+    def set_range(self, start_date: str, stop_date: str):
+        valid_start = self._is_valid_iso8601_utc(start_date)
+        valid_end = self._is_valid_iso8601_utc(stop_date)
         if not valid_start & valid_end:
             raise ValueError("No valid date format - must be yyyy-mm-ddTHH:MM:SSZ")
-        self.range = f'''|> range(start: {start_time}, stop: {stop_time})'''
+        self._range = f'''|> range(start: {start_date}, stop: {stop_date})'''
         return self
 
     def set_measurement(self, measurement: str):
-        self.measurement = f'''|> filter(fn: (r) => r._measurement == "{measurement}")'''
+        self._measurement = f'''|> filter(fn: (r) => r._measurement == "{measurement}")'''
         return self
 
     def set_fields(self, fields: list):
-        self.fields = "|> filter(fn: (r) =>"
+        self._fields = "|> filter(fn: (r) =>"
         for index, field in enumerate(fields):
             if index == 0:
-                self.fields = self.fields + f''' r._field == "{field}"'''
+                self._fields = self._fields + f''' r._field == "{field}"'''
             else:
-                self.fields = self.fields + f''' or r._field == "{field}"'''
-        self.fields = self.fields + ")"
+                self._fields = self._fields + f''' or r._field == "{field}"'''
+        self._fields = self._fields + ")"
         return self
 
     def build(self):
-        if self.bucket is None:
+        if self._bucket is None:
             raise ValueError("bucket must be set!")
-        if self.range is None:
+        if self._range is None:
             raise ValueError("time range must be set!")
-        if self.measurement is None:
+        if self._measurement is None:
             raise ValueError("measurement must be set!")
 
-        self.query = self.bucket + self.range + self.measurement
-        if self.fields is not None:
-            self.query = self.query + self.fields
-        self.query = self.query + self.pivot
+        self.query = self._bucket + self._range + self._measurement
+        if self._fields is not None:
+            self.query = self.query + self._fields
+        self.query = self.query + self._pivot
 
         return self.query
 
