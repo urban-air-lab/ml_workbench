8e427be291b55c1e487d56b2f963735f04083dd9
tobias.weiland <tobias.weiland@7p-group.com>
2025-03-09 09:24:08 +0100

    save runs of pytorch model

--- app/machine_learning/20241116_20250106_sontc_feedforward/train.py
+++ app/machine_learning/20241116_20250106_sontc_feedforward/train.py
@@ -1,3 +1,7 @@
+import json
+import os
+from pathlib import Path
+
 from app.database.Influx_db_connector import InfluxDBConnector
 from app.database.influx_buckets import InfluxBuckets
 from app.database.influx_query_builder import InfluxQueryBuilder
@@ -5,12 +9,14 @@ import pandas as pd
 import torch
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, TensorDataset
-from app.machine_learning.PytorchModels import FeedForwardModel
-from app.utils import calculate_w_a_difference, get_config, align_dataframes_by_time
+from app.machine_learning.PytorchModels import FeedForwardModel, PytorchModel
+from app.utils import calculate_w_a_difference, get_config, align_dataframes_by_time, create_run_directory
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     connection = InfluxDBConnector()
 
+    # TODO: Feuchte und Temperatur hinzufügen
     inputs_query = InfluxQueryBuilder() \
         .set_bucket(InfluxBuckets.AQSN_MINUTE_CALIBRATION_BUCKET.value) \
         .set_range("2024-11-16T00:00:00Z", "2025-01-06T23:00:00Z") \
@@ -37,6 +43,8 @@ if __name__ == "__main__":
     gases = ["NO2", "NO", "O3"]
     align_input_differences = calculate_w_a_difference(align_inputs, gases)
 
+    #TODO: Normalisierung der Daten
+
     inputs_train, inputs_test, targets_train, targets_test = train_test_split(align_input_differences,
                                                         align_targets["NO2"],
                                                         test_size=0.2,
@@ -57,11 +65,29 @@ if __name__ == "__main__":
     test_loader = DataLoader(dataset=TensorDataset(inputs_test_tensor, targets_test_tensor),
                              batch_size=hyperparameters["batch_size"], shuffle=True)
 
-    model = FeedForwardModel(3, hyperparameters["learning_rate"])
+    model: PytorchModel = FeedForwardModel(3, hyperparameters["learning_rate"])
 
     for epoch in range(hyperparameters["epochs"]):
         model.backward(train_loader, epoch, hyperparameters["epochs"])
         model.validate(test_loader)
 
-    predictions = model.forward(inputs_test_tensor)
-    print(predictions)
+    prediction = model.forward(inputs_test_tensor)
+
+    run_directory = create_run_directory()
+
+    parameters = hyperparameters
+    parameters["training_loss"] = model.training_loss
+    parameters["validation_loss"] = model.validation_loss
+    with open(run_directory / Path("parameters.json"), 'w') as convert_file:
+        convert_file.write(json.dumps(parameters))
+
+    compare_dataframe = pd.DataFrame()
+    compare_dataframe["True"] = targets_test
+    compare_dataframe["Prediction"] = prediction.detach().numpy()
+    compare_dataframe.to_csv(run_directory / Path("predictions.csv"))
+
+    plt.plot(compare_dataframe)
+    plt.savefig(run_directory / Path("predictions.png"))
+
+
+
