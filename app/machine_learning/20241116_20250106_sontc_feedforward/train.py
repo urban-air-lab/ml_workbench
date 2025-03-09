215a92fc2dad9a6a6c10704d21e7bd561bc820c5
tobias.weiland <tobias.weiland@7p-group.com>
2025-03-09 19:59:41 +0100

    fix imports

--- app/machine_learning/20241116_20250106_sontc_feedforward/train.py
+++ app/machine_learning/20241116_20250106_sontc_feedforward/train.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     inputs_query = InfluxQueryBuilder() \
         .set_bucket(InfluxBuckets.AQSN_MINUTE_CALIBRATION_BUCKET.value) \
         .set_range("2024-11-16T00:00:00Z", "2025-01-06T23:00:00Z") \
-        .set_measurement(Sensors.AQSNSensors.SONT_C.value) \
+        .set_measurement(sensors.AQSNSensors.SONT_C.value) \
         .set_fields(["RAW_ADC_NO2_W",
                      "RAW_ADC_NO2_A",
                      "RAW_ADC_NO_W",
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     target_query = InfluxQueryBuilder() \
         .set_bucket(InfluxBuckets.LUBW_MINUTE_BUCKET.value) \
         .set_range("2024-11-16T00:00:00Z", "2025-01-06T23:00:00Z") \
-        .set_measurement(Sensors.LUBWSensors.DEBW015.value) \
+        .set_measurement(sensors.LUBWSensors.DEBW015.value) \
         .set_fields(["NO2"]) \
         .build()
     target_data = connection.query_dataframe(target_query)
