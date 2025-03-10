2e4f42aaf9fc3638a8487e8040e91d1c7ca1f705
tobias.weiland <tobias.weiland@7p-group.com>
2025-03-10 19:54:47 +0100

    delete ARIMA model

--- app/machine_learning/models_basic.py
+++ app/machine_learning/models_basic.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 from sklearn.ensemble import RandomForestRegressor
-from statsmodels.tsa.arima.model import ARIMA
 
 
 class MachineLearningModel(ABC):
@@ -11,18 +10,6 @@ class MachineLearningModel(ABC):
     @abstractmethod
     def predict(self, x):
         pass
-
-
-class ARIMAModel(MachineLearningModel):
-    def __init__(self, p=1, d=1, q=1):
-        self.regressor = ARIMA(p, d, q)
-        self.forecaster = None
-
-    def fit(self, x, y):
-        self.forecaster = self.regressor.fit()
-
-    def predict(self, x):
-        return self.forecaster.forecast()
 
 
 class RandomForestModel(MachineLearningModel):
