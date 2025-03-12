ea12444b8d390ae7cab52cf83c64e599ed3def92
tobias.weiland <tobias.weiland@7p-group.com>
2025-03-12 20:01:25 +0100

    add comments

--- app/database/sensors.py
+++ app/database/sensors.py
@@ -2,11 +2,18 @@ from enum import Enum
 
 
 class LUBWSensors(Enum):
+    """
+    All current in use sensors from lubw
+    debw is english abbreviation for lubw
+    """
     DEBW015 = "DEBW015"
     DEBW152 = "DEBW152"
 
 
 class AQSNSensors(Enum):
+    """
+    All current in use custom sensors from AirUP! project
+    """
     SONT_A = "sont_a"
     SONT_B = "sont_b"
     SONT_C = "sont_c"
