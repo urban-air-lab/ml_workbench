f1156a32985dca3b9a41850f6ca3fe77f82bd103
Tobias Weiland <post@tobias-weiland.eu>
2025-05-11 10:54:15 +0200

    run 20241114_20250107_ual-1 hourly results for NO2

---
+++ app/machine_learning/20241114_20250107_ual-1/hour_results/xgboost_NO2/train.py
@@ -0,0 +1,15 @@
+import xgboost as xgb
+from app.machine_learning.workflows import workflow20241114_20250107_ual1_2
+
+if __name__ == "__main__":
+    inputs = ["RAW_ADC_NO_W",
+             "RAW_ADC_NO_A",
+             "RAW_ADC_NO2_W",
+             "RAW_ADC_NO2_A",
+             "RAW_ADC_O3_W",
+             "RAW_ADC_O3_A",
+             "sht_humid",
+             "sht_temp"]
+    target = ["NO2"]
+    model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=10)
+    workflow20241114_20250107_ual1_2(inputs, target, model)
